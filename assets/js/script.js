166bcc7ae0f8b947dd249fb5d859b3f1808054f0
Tevissaur <berserkboar@protonmail.com>
2021-08-25 16:32:22 -0500

    adding svg and generating card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 // Element Selections
 let searchButton = document.getElementById('search')
-let cloud = document.getElementById('cloud')
-let sun = document.querySelector('div.sun')
+let cloud = function() {
+    let cloud = document.createElement('div')
+    cloud.classList.add('cloud', 'float')
+    return cloud
+}
+let sun = function() {
+    let sun = document.createElement('div')
+    sun.classList.add('sun')
+    return sun
+}
 let citiesDataList = document.getElementById('cities')
 let searchInput = document.getElementById('city-input')
+let todaysWeather = document.getElementById('todays-weather')
+
+
 // API Url and Keys
 let baseUrl = 'https://api.openweathermap.org/data/2.5/onecall'
 let APIkey = '99614a3bd71c4f4912965f0c6b55b111'
@@ -20,6 +31,29 @@ for (let i in cities) {
     citiesDataList.appendChild(optionsElem)
 }
 
+function createWeatherCard(city, temp, wind, humidity, UVindex, weather) {
+    let paramsArray = [temp, wind, humidity, UVindex, weather]
+    let weatherCard = document.createElement('div')
+    weatherCard.classList.add('card', 'm-5', 'w-50')
+    let cardHeader = document.createElement('div')
+    cardHeader.classList.add('card-header')
+    let cardTitle = document.createElement('h3')
+    cardTitle.classList.add('card-title')
+    cardTitle.textContent = city
+    cardHeader.appendChild(cardTitle)
+    weatherCard.appendChild(cardHeader)
+    let cardList = document.createElement('ul')
+    cardList.classList.add('list-group','list-group-flush')
+    for (let i in paramsArray) {
+        let listItem = document.createElement('li')
+        listItem.classList.add('list-group-item')
+        listItem.textContent = i
+        cardList.appendChild(listItem)
+    }
+    weatherCard.appendChild(cardList)
+    todaysWeather.appendChild(weatherCard)
+}
+createWeatherCard('Detroit',1,2,3,4,5)
 // TO CONVERT UNIX EPOCH TO MS  =>  timeInS * 1000
 
 // Sends request
@@ -31,18 +65,21 @@ searchButton.addEventListener('click', function (e) {
         let lat = cities[city]['lat']
         let lon = cities[city]['lon']
         console.log(city)
-        let customCall = `?lat=${lat}&lon=${lon}&appid=${APIkey}`
+        let customCall = `?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${APIkey}`
         fetch(baseUrl + customCall)
             .then(response => {
                 return response.json()
             })
             .then(data => {
+                if (data['alerts']) {
+                    let alerts = data['alerts']
+
+                }
                 console.log(data)
             })
-            cloud.classList.add('away')
-            sun.classList.add('away')
-    }
-    catch(err) {
-        console.log(err)
-    }
+        }
+        catch(err) {
+            console.log(err)
+        }
+        searchInput.value = ''
 })
